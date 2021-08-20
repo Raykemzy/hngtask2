28e7ef520c434c317a8ce0823b63fde9af6d8e95
Raymond <ukemzyukofia@gmail.com>
2021-08-20 14:25:21 +0100

    first commit

---
+++ app.dart
@@ -0,0 +1,7 @@
+void main() {
+  String name = 'Ukeme Raymond Ukofia';
+  String track = 'Mobile(Flutter)';
+
+  print(
+      "Hi, my name is $name and my track is $track. \nHere is my task submission");
+}
