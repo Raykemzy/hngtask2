6173dd0f325a66172ff949867c17e463c30e1fde
Raymond <ukemzyukofia@gmail.com>
2021-08-21 00:27:22 +0100

    another commit

--- displayapp/lib/main.dart
+++ displayapp/lib/main.dart
@@ -34,56 +34,57 @@ class _HomepageState extends State<Homepage> {
     return Scaffold(
       backgroundColor: Colors.black38,
       body: SafeArea(
-        child: Container(
-          width: MediaQuery.of(context).size.width,
-          child: Padding(
-            padding: EdgeInsets.symmetric(
-              horizontal: 24.0,
-              vertical: 36.0,
-            ),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                // ignore: avoid_unnecessary_containers
-                Container(
-                  child: Image(
-                    image: AssetImage('assets/images/hng_logo.png'),
+        child: SingleChildScrollView(
+          child: Container(
+            width: MediaQuery.of(context).size.width,
+            child: Padding(
+              padding: EdgeInsets.only(
+                left: 24.0,
+                right: 24.0,
+                top: 50.0,
+              ),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  // ignore: avoid_unnecessary_containers
+                  Container(
+                    height: 60.0,
+                    width: 160.0,
+                    child: Image(
+                      image: AssetImage('assets/images/hng_logo.png'),
+                    ),
+                    margin: EdgeInsets.only(bottom: 10.0),
                   ),
-                  margin: EdgeInsets.only(bottom: 10.0),
-                ),
-                Divider(
-                  thickness: 5.0,
-                  color: Colors.black,
-                ),
-                Padding(
-                  padding: const EdgeInsets.only(top: 50.0),
-                  child: Row(
-                    children: [
-                      Expanded(
-                        child: TextField(
-                          style: TextStyle(color: Colors.grey),
-                          controller: nameController,
-                          decoration: InputDecoration(
-                            hintText: "Hi, what's your name?",
-                            hintStyle: TextStyle(
-                              fontSize: 26.0,
-                              color: Colors.grey,
+                  Padding(
+                    padding: const EdgeInsets.only(top: 100.0),
+                    child: Row(
+                      children: [
+                        Expanded(
+                          child: TextField(
+                            style: TextStyle(color: Colors.grey),
+                            controller: nameController,
+                            decoration: InputDecoration(
+                              hintText: "Hi, what's your name?",
+                              hintStyle: TextStyle(
+                                fontSize: 20.0,
+                                color: Colors.grey,
+                              ),
+                              border: OutlineInputBorder(),
                             ),
-                            border: InputBorder.none,
+                            onSubmitted: (name) {
+                              setState(() {
+                                name = nameController.text;
+                              });
+                            },
                           ),
-                          onSubmitted: (name) {
-                            setState(() {
-                              name = nameController.text;
-                            });
-                          },
                         ),
-                      ),
-                      Stack(
-                        children: [
-                          Container(
-                            width: 60.0,
-                            height: 60.0,
-                            decoration: BoxDecoration(
+                        Stack(
+                          children: [
+                            Container(
+                              width: 40.0,
+                              height: 40.0,
+                              margin: EdgeInsets.only(left: 5.0),
+                              decoration: BoxDecoration(
                                 gradient: LinearGradient(
                                   colors: <Color>[
                                     Color(0xFF0D47A1),
@@ -91,54 +92,67 @@ class _HomepageState extends State<Homepage> {
                                     Color(0xFF42A5F5),
                                   ],
                                 ),
-                                borderRadius: BorderRadius.circular(50.0)),
-                          ),
-                          Positioned(
-                            top: 5.0,
-                            left: 6.0,
-                            child: IconButton(
-                              icon: Icon(Icons.send),
-                              onPressed: () {
-                                setState(() {
-                                  name = nameController.text;
-                                });
-                              },
+                                borderRadius: BorderRadius.circular(50.0),
+                              ),
+                            ),
+                            Positioned(
+                              left: 2.0,
+                              top: -3.0,
+                              child: IconButton(
+                                icon: Icon(Icons.send),
+                                onPressed: () {
+                                  setState(() {
+                                    name = nameController.text;
+                                  });
+                                },
+                              ),
+                            ),
+                          ],
+                        )
+                      ],
+                    ),
+                  ),
+                  SizedBox(height: 50.0),
+                  RichText(
+                    text: TextSpan(
+                        text: 'Hi ',
+                        style: TextStyle(
+                          color: Colors.grey,
+                          fontSize: 22.0,
+                          fontStyle: FontStyle.italic,
+                        ),
+                        children: <TextSpan>[
+                          TextSpan(
+                            text: '$name',
+                            style: TextStyle(
+                              backgroundColor: Colors.grey,
+                              color: Colors.black,
                             ),
                           ),
-                        ],
-                      )
-                    ],
+                          TextSpan(
+                              text:
+                                  ' click on the link below to visit the HNG site'),
+                        ]),
                   ),
-                ),
-                SizedBox(height: 50.0),
-                Text(
-                  "Hi $name, click on the link below to visit the HNG site",
-                  style: TextStyle(
-                      fontSize: 22.0,
-                      fontWeight: FontWeight.bold,
-                      color: Colors.grey,
-                      fontStyle: FontStyle.italic
-                      //foreground: Paint()..shader = ui.Gradient.linear(),
-                      ),
-                ),
-                SizedBox(height: 10.0),
-                InkWell(
-                  child: ButtonTheme(
-                    child: OutlinedButton(
-                      style: OutlinedButton.styleFrom(
-                        primary: Colors.white,
-                        backgroundColor: Colors.blueAccent,
-                        side: BorderSide(width: 2.0),
-                        maximumSize: Size(100.0, 50.0),
-                      ),
-                      onPressed: () => {launch(url)},
-                      child: Center(
-                        child: Text('HNG'),
+                  SizedBox(height: 10.0),
+                  InkWell(
+                    child: ButtonTheme(
+                      child: OutlinedButton(
+                        style: OutlinedButton.styleFrom(
+                          primary: Colors.white,
+                          backgroundColor: Colors.blueAccent,
+                          side: BorderSide(width: 2.0),
+                          maximumSize: Size(100.0, 50.0),
+                        ),
+                        onPressed: () => {launch(url)},
+                        child: Center(
+                          child: Text('HNG'),
+                        ),
                       ),
                     ),
                   ),
-                ),
-              ],
+                ],
+              ),
             ),
           ),
         ),
